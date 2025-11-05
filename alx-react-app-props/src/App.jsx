916bc981962f410e85878037b447c66a055bc45e
Hosam Elshaboury <hosam.elshaboury@gmail.com>
2025-11-06 01:57:22 +0200

    compelete task2

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import UserContext from './UserContext';
-import UserProfile from './components/UserProfile';
+import UserProfile from './UserProfile';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
