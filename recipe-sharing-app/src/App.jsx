fa3dd3657a40979ad49764138a056605c8c2dc2f
Hosam Elshaboury <hosam.elshaboury@gmail.com>
2025-11-17 08:09:57 +0200

    modified App.jsx to pass the checker

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,32 +1,34 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Recipe Sharing App</h1>
+    <Router>
+      <div style={{ padding: "20px" }}>
+        <h1>Recipe Sharing App</h1>
 
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <hr />
-              <RecipeList />
-            </>
-          }
-        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <hr />
+                <RecipeList />
+              </>
+            }
+          />
 
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
