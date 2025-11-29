b9a7ab9514da32501dd1fc16c5be7ed984e3ae85
Hosam Elshaboury <hosam.elshaboury@gmail.com>
2025-11-29 19:34:25 +0200

    complete task2

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -4,10 +4,9 @@ import Search from "./components/Search";
 export default function App() {
   return (
     <div>
-      <h1 style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1 className="text-3xl font-bold text-center mt-6">
         GitHub User Search
       </h1>
-
       <Search />
     </div>
   );
