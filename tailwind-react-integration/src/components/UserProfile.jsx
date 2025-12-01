4c4de2782f5a4614148baf6ccdbc9fa36e342c12
Hosam Elshaboury <hosam.elshaboury@gmail.com>
2025-12-01 14:20:28 +0200

    complete task2

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,17 +1,17 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto"
+        className="w-24 md:w-36 h-24 md:h-36 rounded-full mx-auto"
       />
       
-      <h1 className="text-lg sm:text-xl text-blue-800 my-4">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4">
         John Doe
       </h1>
 
-      <p className="text-sm sm:text-base text-gray-600">
+      <p className="text-sm md:text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
