4c0f617859d53aca6484a444d90397f46b6c40ef
Hosam Elshaboury <hosam.elshaboury@gmail.com>
2025-12-01 14:35:41 +0200

    complete task3

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center 
+                    hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto"
+        className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto
+                   hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       
-      <h1 className="text-lg md:text-xl text-blue-800 my-4">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">
         John Doe
       </h1>
 
